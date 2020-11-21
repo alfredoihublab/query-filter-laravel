2aa72f498da8310e4b05c04caf72f019888b852f
Felipe Guzman <felipe-guzman.c@hotmail.com>
2020-11-21 22:36:13 +0000

    add commands make filter and maker query class

--- src/QueryFilterServiceProvider.php
+++ src/QueryFilterServiceProvider.php
@@ -1,12 +1,19 @@
 <?php
 namespace Fguzman;
 
-use Illuminate\Support\Facades\Blade;
+use Fguzman\Commands\MakeQuery;
+use Fguzman\Commands\MakeFilter;
 use Illuminate\Support\ServiceProvider;
 
 class QueryFilterServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                MakeQuery::class,
+                MakeFilter::class
+            ]);
+        }
     }
 }
